08037013422d446afba1c1a71f52db5512f06422
Restyled.io <commits@restyled.io>
2021-06-24 08:43:40 +0000

    Restyled by prettier

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,68 +3,69 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  exclude : [
-    '**/node_modules/**/*',
-    'src/_data',
-    'src/**/*.md',
-    'src/**/*.njk',
-    'src/**/*.ejs',
-  ],
-  mount : {
-    public : {url : '/', static : true},
-    'src/scripts' : '/scripts',
-    'src/styles' : '/styles',
-    'src/images' : '/images',
-  },
-  // optimize: {
-  //     bundle: true,
-  //     minify: true,
-  //     target: 'es2017',
-  //     treeshake: true,
-  // },
-  plugins : [
-    [ '@snowpack/plugin-babel' ], '@snowpack/plugin-postcss',
-    // [
-    //   '@snowpack/plugin-build-script',
-    //   {
-    //     cmd: 'babel --filename $FILE', // cmd to run
-    //   },
-    // ],
-    [
-      '@snowpack/plugin-run-script',
-      {
-        cmd : 'eleventy',
-        watch : '$1 --watch',
-      },
+    exclude: [
+        '**/node_modules/**/*',
+        'src/_data',
+        'src/**/*.md',
+        'src/**/*.njk',
+        'src/**/*.ejs',
     ],
-    // [
-    //     '@snowpack/plugin-webpack',
-    //     {
-    //         outputPattern: {
-    //             css: 'main.css',
-    //             js: '[filename].js',
-    //         },
-    //         extendConfig: (config) => {
-    //             config.plugins.push(/* ... */);
-    //             return config;
-    //         },
-    //         sourceMap: false,
-    //     },
-    // ],
-  ],
-  routes : [
-    /* Enable an SPA Fallback in development: */
-    {match : 'routes', src : '.*', dest : '/index.html'},
-  ],
-  packageOptions : {
-    source : 'remote',
-  },
-  devOptions : {
-      // https://github.com/tailwindlabs/tailwindcss/issues/3950#issuecomment-846967828
-      // tailwindConfig: './tailwind.config.js',
-  },
-  buildOptions : {
-    hmrDelay : 300,
-    /* ... */
-  },
+    mount: {
+        public: { url: '/', static: true },
+        'src/scripts': '/scripts',
+        'src/styles': '/styles',
+        'src/images': '/images',
+    },
+    // optimize: {
+    //     bundle: true,
+    //     minify: true,
+    //     target: 'es2017',
+    //     treeshake: true,
+    // },
+    plugins: [
+        ['@snowpack/plugin-babel'],
+        '@snowpack/plugin-postcss',
+        // [
+        //   '@snowpack/plugin-build-script',
+        //   {
+        //     cmd: 'babel --filename $FILE', // cmd to run
+        //   },
+        // ],
+        [
+            '@snowpack/plugin-run-script',
+            {
+                cmd: 'eleventy',
+                watch: '$1 --watch',
+            },
+        ],
+        // [
+        //     '@snowpack/plugin-webpack',
+        //     {
+        //         outputPattern: {
+        //             css: 'main.css',
+        //             js: '[filename].js',
+        //         },
+        //         extendConfig: (config) => {
+        //             config.plugins.push(/* ... */);
+        //             return config;
+        //         },
+        //         sourceMap: false,
+        //     },
+        // ],
+    ],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        { match: 'routes', src: '.*', dest: '/index.html' },
+    ],
+    packageOptions: {
+        source: 'remote',
+    },
+    devOptions: {
+        // https://github.com/tailwindlabs/tailwindcss/issues/3950#issuecomment-846967828
+        // tailwindConfig: './tailwind.config.js',
+    },
+    buildOptions: {
+        hmrDelay: 300,
+        /* ... */
+    },
 };
